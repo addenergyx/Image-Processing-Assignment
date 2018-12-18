e8727972a620d498ecbd01bb518e77aed5575e57
David <david-adeniji@hotmail.co.uk>
2018-12-18 11:09:32 +0000

    variable file rename

--- A3_thresholding.m
+++ A3_thresholding.m
@@ -2,35 +2,35 @@ clear all;
 close all;
 clc;
 
-palmDownImageFile = imread('flat.png');
+hand = imread('flat.png');
 
 
-r = palmDownImageFile(:, :, 1); 
-g = palmDownImageFile(:, :, 2); 
-b = palmDownImageFile(:, :, 3); 
+r = hand(:, :, 1); 
+g = hand(:, :, 2); 
+b = hand(:, :, 3); 
 
 
 orangeThumbBinary = r > 142 & r < 208 & g > 68 & g < 109 & b < 54; 
-orangeThumbIsolated = palmDownImageFile;
+orangeThumbIsolated = hand;
 orangeThumbIsolated(repmat(~orangeThumbBinary,[1 1 3])) = 0; 
 
 
 yellowIndexBinary = r > 174 & r < 235 & g > 142 & g < 206 & b > 33 & b < 108;
-yellowIndexIsolated = palmDownImageFile;
+yellowIndexIsolated = hand;
 yellowIndexIsolated(repmat(~yellowIndexBinary,[1 1 3])) = 0;
 
 
 blueMiddleBinary = r > 14 & r < 44 & g > 64 & g < 90 & b > 130 & b < 149;
-blueMiddleIsolated = palmDownImageFile;
+blueMiddleIsolated = hand;
 blueMiddleIsolated(repmat(~blueMiddleBinary,[1 1 3])) = 0;
 
 
 greenRingBinary = r < 12 & g > 93 & g < 130 & b > 97 & b < 130;
-greenRingIsolated = palmDownImageFile;
+greenRingIsolated = hand;
 greenRingIsolated(repmat(~greenRingBinary,[1 1 3])) = 0;
 
 redPinkyBinary = r > 200 & r < 223 & g > 99 & g < 136 & b > 84 & b < 119;
-redPinkyIsolated = palmDownImageFile;
+redPinkyIsolated = hand;
 redPinkyIsolated(repmat(~redPinkyBinary,[1 1 3])) = 0;
 
 figure;
